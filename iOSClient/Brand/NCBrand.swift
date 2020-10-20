2deca23309feb1661fc9fed802e1febd72fc91b6
CharlesEtieve <charles.etieve@gmail.com>
2020-10-21 00:47:30 +0900

    Fix AppGroup

--- iOSClient/Brand/NCBrand.swift
+++ iOSClient/Brand/NCBrand.swift
@@ -64,7 +64,7 @@ import UIKit
     @objc public var folderDefaultAutoUpload:           String = "Photos"
     
     // Capabilities Group
-    @objc public var capabilitiesGroups:                String = "group.it.twsweb.Crypto-Cloud"
+    @objc public var capabilitiesGroups:                String = "group.leviia.app"
     
     // User Agent
     //@objc public var userAgent:                         String = "Nextcloud-iOS"                                    // Don't touch me !!
